e101f2ae17088efbc1359daa6f8a4b1203dbcad4
Keny Pau Vas <keny_pauccarima@usmp.pe>
2023-01-17 12:44:08 -0500

    ...

--- Simple Banking System/task/src/banking/Main.java
+++ Simple Banking System/task/src/banking/Main.java
@@ -1,54 +1,67 @@
 package banking;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-enum MenuOpt{
+enum MenuOpt {
     CREATE(1),
     LOG(2),
     EXIT(3),
     DEFAULT(4);
-    int value ;
-    MenuOpt(int value){
-        this.value=value;
+    final int value;
+
+    MenuOpt(int value) {
+        this.value = value;
     }
 
-    public int getValue() {
-        return value;
-    }
+
 }
-enum BankingSystem{
+
+enum BankingSystem {
     BIN("400000");
-    String value;
-    BankingSystem(String value){
+    final String value;
+
+    BankingSystem(String value) {
         this.value = value;
     }
 }
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static List<Account> accounts = new ArrayList<>();
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        showMenu();
-        int inputUser=inputUserMenu(scanner);
-        menuBank(inputUser);
-    }
-    public static void showMenu(){
-        System.out.printf("1. Create an account%n2. Log into account%n0. Exit%n");
+        int inputUser;
+        do {
+            showMenu();
+            inputUser = inputUserMenu();
+            menuBank(inputUser);
+        } while (inputUser != 0);
 
     }
-    public static int inputUserMenu(Scanner scanner){
+
+    public static void showMenu() {
+        System.out.printf("1. Create an account%n2. Log into account%n0. Exit%n");
+    }
+
+    public static int inputUserMenu() {
         return scanner.nextInt();
     }
-    public static void menuBank(int userInput){
+
+    public static void menuBank(int userInput) {
 
         MenuOpt option = menu(userInput);
-        switch (option){
-            case CREATE -> System.out.println("TODO create method");
-            case LOG -> System.out.println("TODO Log method");
+        switch (option) {
+            case CREATE -> createBankAccount();
+            case LOG -> logAccount();
             case EXIT -> System.out.println("TODO exit");
             default -> System.out.println("Invalid input!");
         }
     }
-    public static MenuOpt menu(int userInput){
+
+    public static MenuOpt menu(int userInput) {
         switch (userInput) {
             case 1 -> {
                 return MenuOpt.CREATE;
@@ -64,8 +77,136 @@ public class Main {
             }
         }
     }
-    public static String randomNumberAccountGenerate(){
+
+    public static void createBankAccount() {
+        boolean validCard = false;
+
+        String cardNumber;
+        String pinNumber;
+        do {
+            cardNumber = BankingSystem.BIN.value.concat(randomNumberAccountGenerate());
+            validCard = searchAccount(cardNumber);
+            pinNumber = randomPinGenerate();
+        } while (validCard);
+
+        accounts.add(new Account(cardNumber, pinNumber));
+        showSuccesFullMessage(cardNumber, pinNumber);
+    }
+
+    public static void showSuccesFullMessage(String cardNumber, String pin) {
+        System.out.printf("Your card has been created%nYour card number:%n%s%nYour card PIN:%n%s%n", cardNumber, pin);
+    }
+
+    public static boolean searchAccount(String cardNumber) {
+        for (Account x : accounts) {
+            if (x.getCardNumber().equals(cardNumber)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean searchPinNumber(String pinNumber) {
+        for (Account x : accounts) {
+            if (x.getCardPIN().equals(pinNumber)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String randomNumberAccountGenerate() {
         Random random = new Random();
-        return " ";
+        int max = 11000;
+        int min = 10000;
+
+        int account1 = random.nextInt(max - min + 1) + min;
+        int account2 = random.nextInt(max - min + 1) + min;
+
+        return account1 + "" + account2;
+    }
+
+    public static String randomPinGenerate() {
+        Random random = new Random();
+        return String.valueOf(random.nextInt(9999 - 1000 + 1) + 1000);
+    }
+
+    public static void logAccount() {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Enter your card number:%n");
+        String cardNumber = sc.nextLine();
+        System.out.printf("Enter your pin number:%n");
+        String pinNumber = sc.nextLine();
+
+        if (!searchAccount(cardNumber) && searchPinNumber(pinNumber)) {
+            System.out.printf("Wrong card number or PIN!%n");
+        } else {
+            System.out.printf("You have successfully logged in!%n");
+            menuUserLogged(cardNumber);
+        }
+    }
+
+    public static void menuUserLogged(String cardNumber) {
+        System.out.printf("1. Balance%n2. Log out%n0. Exit%n");
+        switch (scanner.nextInt()) {
+            case 1:
+                balance(cardNumber);
+                break;
+            case 2:
+                System.out.println("You have successfully logged out!");
+            case 0:
+                System.out.println("Exit");
+        }
+    }
+
+    public static void balance(String cardNumber) {
+        Account account = accounts.stream()
+                .filter(x -> x.getCardNumber().equals(cardNumber))
+                .findFirst()
+                .orElse(null);
+        System.out.printf("Balance: %f%n", account.getBalance());
+    }
+}
+
+class Account {
+    private String cardNumber;
+    private String cardPIN;
+    private double balance;
+
+    public Account(String account, String pin) {
+        this.cardNumber = account;
+        this.cardPIN = pin;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardPIN() {
+        return cardPIN;
+    }
+
+    public void setCardPIN(String cardPIN) {
+        this.cardPIN = cardPIN;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "account='" + cardNumber + '\'' +
+                ", pin='" + cardPIN + '\'' +
+                '}';
     }
 }
